339fc459fe6de33ab1aa570b22f5c36e55fffefa
LilianMichel <55470126+LilianMichel@users.noreply.github.com>
2020-03-25 09:40:04 -0700

    OpcionEliminar

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../models/ingredient.service';
+import { IngredientService } from '../services/ingredient.service';
+import { Router } from '@angular/router';
+import { AlertController} from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +12,78 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  ingredients: Ingredient[];
+  search: string;
+
+  constructor(private studentService: IngredientService, private router: Router, private alert: AlertController) {
+    this.clearSearch();
+  }
+
+  operation(pos: number, ev: {detail: { side}}) {
+    const side = ev.detail.side;
+    if (side === 'start') {
+      this.studentService.changeStatus(pos);
+    } else {
+      this.showAlert(pos);
+    }
+  }
+  async showAlert(pos: number) {
+    const al = await this.alert.create({
+      header: 'Confirmar',
+      message: '¿Seguro que desea eliminar?',
+      buttons: [{
+        text: 'No',
+        handler: () => {}
+      }, {
+        text: 'Si',
+        handler: () => {
+          this.studentService.deleteIngredient(pos);
+        }
+      }]
+    });
+    await al.present();
+  }
+
+  newIngredient(): void {
+    this.router.navigate(['/new-ingredient']);
+  }
+
+  clearSearch(): void {
+    this.ingredients = this.studentService.getIngredients();
+  }
+  filter(): void {
+    this.clearSearch();
+
+    if (this.search && this.search.trim()) {
+      this.ingredients = this.ingredients.filter( (student) => {
+        return ((student.name.toLocaleLowerCase().indexOf(this.search.toLocaleLowerCase()) > -1)  ||
+         (student.imagen.toLocaleLowerCase().indexOf(this.search.toLocaleLowerCase()) > -1));
+      });
+}
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
 
 }
